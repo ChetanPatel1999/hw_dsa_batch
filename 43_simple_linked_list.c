e12935a63e903ed70df650310f2adcae20e0630a
ChetanPatel1999 <chetan8085patel@gmail.com>
2024-11-15 19:03:42 +0530

    11/15

--- 43_simple_linked_list.c
+++ 43_simple_linked_list.c
@@ -1,12 +1,37 @@
-//simple linked list
-#include<stdio.h>
+// simple linked list
+#include <stdio.h>
+#include<stdlib.h>
 struct node
 {
    int data;
    struct node *next;
 };
+void display(struct node *head)
+{
+   struct node * temp;
+   temp=head;
+   printf("linked list element : ");
+   while(temp!=NULL)
+   {
+      printf("%d ",temp->data);
+      temp=temp->next;
+   }
+}
 void main()
 {
- struct node head;
- printf("%d ",sizeof(struct node));
+   struct node *head, *second, *third, *fourth;
+   head = (struct node *)malloc(sizeof(struct node));
+   second = (struct node *)malloc(sizeof(struct node));
+   third = (struct node *)malloc(sizeof(struct node));
+   fourth = (struct node *)malloc(sizeof(struct node));
+   head->data=12;
+   head->next=second;
+   second->data=34;
+   second->next=third;
+   third->data=56;
+   third->next=fourth;
+   fourth->data=78;
+   fourth->next=NULL;
+   display(head);
 }
+   
