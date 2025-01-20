b46fc102f2099378358aabbff8548066e92c5c8a
ChetanPatel1999 <chetan8085patel@gmail.com>
2025-01-20 19:05:17 +0530

    20/01

--- 66_avl_deletion.c
+++ 66_avl_deletion.c
@@ -1,4 +1,4 @@
-//AVL insertion
+// AVL insertion
 #include <stdio.h>
 #include <stdlib.h>
 struct node
@@ -113,6 +113,83 @@ void pre_order(struct node *root)
         pre_order(root->right);
     }
 }
+struct node *inorder_succesor(struct node *root)
+{
+    while (root->left != NULL)
+    {
+        root = root->left;
+    }
+    return root;
+}
+struct node *delete_node(struct node *root, int data)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    // serach node
+    if (data < root->data)
+    {
+        root->left = delete_node(root->left, data);
+    }
+    else if (data > root->data)
+    {
+        root->right = delete_node(root->right, data);
+    }
+    else
+    {
+        // case 1 : leaf node
+        // case 2: only have one child
+        if (root->right == NULL)
+        {
+            struct node *temp = root->left;
+            free(root);
+            return temp;
+        }
+        else if (root->left == NULL)
+        {
+            struct node *temp = root->right;
+            free(root);
+            return temp;
+        }
+        else
+        {
+            // root node or node have two child
+            struct node *temp = inorder_succesor(root->right);
+            root->data = temp->data;
+            root->right = delete_node(root->right, temp->data);
+        }
+    }
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    root->height = 1 + max(get_height(root->left), get_height(root->right));
+    int bf = get_bf(root);
+    // LL rotation
+    if (bf > 1 && get_bf(root->left) >= 0)
+    {
+        return LL_rotation(root);
+    }
+    // LR rotation
+    if (bf > 1 && get_bf(root->left) < 0)
+    {
+        root->left = RR_rotation(root->left);
+        return LL_rotation(root);
+    }
+    // RR rotation
+    if (bf < -1 && get_bf(root->right) <= 0)
+    {
+        return RR_rotation(root);
+    }
+    // RL rotation
+    if (bf < -1 && get_bf(root->right) > 0)
+    {
+        root->right = LL_rotation(root->right);
+        return RR_rotation(root);
+    }
+    return root;
+}
 void main()
 {
     struct node *root = NULL;
@@ -123,4 +200,9 @@ void main()
     root = insert(root, 6);
     root = insert(root, 3);
     pre_order(root);
+    printf("\n");
+    root = delete_node(root, 5);
+    root = delete_node(root, 6);
+    pre_order(root);
+    printf("\n");
 }
