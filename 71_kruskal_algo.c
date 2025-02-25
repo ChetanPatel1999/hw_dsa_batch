fd56be5105fb33354faf28a527491deb848cf72e
ChetanPatel1999 <chetan8085patel@gmail.com>
2025-02-25 19:09:23 +0530

    25/02

--- 71_kruskal_algo.c
+++ 71_kruskal_algo.c
@@ -5,6 +5,45 @@ struct edge
     int v;
     int weight;
 };
+int parent[50], n = 7;
+int find(int i)
+{
+    if (parent[i] == -1)
+    {
+        return i;
+    }
+    return parent[i] = find(parent[i]);
+}
+int uni(int u, int v)
+{
+    int rootU = find(u);
+    int rootV = find(v);
+    if (rootU != rootV)
+    {
+        parent[rootV] = rootU;
+        return 1;
+    }
+    return 0;
+}
+void kruskal(struct edge ed[], int edge_count)
+{
+    int ne = 0, i, mincost = 0;
+    for (i = 0; i < n; i++)
+    {
+        parent[i] = -1;
+    }
+    printf("\nspannig tree edges :\n");
+    printf("edno.   edges    weight \n");
+    for (i = 0; i < edge_count && ne < (n - 1); i++)
+    {
+        if (uni(ed[i].u, ed[i].v))
+        {
+            printf("%d   %d<------->%d    %d\n", ++ne, ed[i].u, ed[i].v, ed[i].weight);
+            mincost = mincost + ed[i].weight;
+        }
+    }
+    printf("minimum cost of graph :%d", mincost);
+}
 void main()
 {
     int adjmat[7][7] = {
@@ -18,7 +57,7 @@ void main()
     };
     struct edge ed[50];
     int i, j, edge_count = 0;
-    for (i = 0; i < 7; i++)
+    for (i = 0; i < 7; i++) // 1
     {
         for (j = 0; j < 7; j++)
         {
@@ -34,9 +73,33 @@ void main()
     }
 
     // code for diplay edges
-    printf("all edges set of graph :-\n");
+    printf("before sorting all edges set of graph :-\n");
+    printf("edno.   edges    weight \n");
     for (i = 0; i < edge_count; i++)
     {
         printf("%d   %d<------>%d    %d \n", i + 1, ed[i].u, ed[i].v, ed[i].weight);
     }
+
+    // code for sorting edges using bubble sort
+    for (i = 0; i < edge_count - 1; i++) // 9
+    {
+        for (j = 0; j < edge_count - i - 1; j++)
+        {
+            if (ed[j].weight > ed[j + 1].weight)
+            {
+                struct edge temp;
+                temp = ed[j];
+                ed[j] = ed[j + 1];
+                ed[j + 1] = temp;
+            }
+        }
+    }
+    // code for diplay edges
+    printf("after sorting all edges set of graph :-\n");
+    printf("edno.   edges    weight \n");
+    for (i = 0; i < edge_count; i++)
+    {
+        printf("%d   %d<------>%d    %d \n", i + 1, ed[i].u, ed[i].v, ed[i].weight);
+    }
+    kruskal(ed, edge_count);
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,8 +1,1 @@
-   struct node *root = NULL;
-    root = insertnode(root, 45);
-    root = insertnode(root, 15);
-    root = insertnode(root, 79);
-    root = insertnode(root, 10);
-    root = insertnode(root, 20);
-    root = insertnode(root, 55);
-    root = insertnode(root, 90);
+7
